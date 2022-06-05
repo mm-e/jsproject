506fd45a17cc61845d64c84fd96f4bb8dc148a8e
mm-e <magdaeaton@hotmail.com>
2022-06-05 10:54:56 -0300

    added funcions

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
-let portmanteau = prompt ("Welcome! Do you know what a PORTMANTEAU is? Please type Y or N");
+let userName = prompt("Hi! My name is PT. What's your name?")
+function identifyUser() { //Funcion sin parametros    
+    alert(`Hi, ${userName} , Welcome!`);
+   }
+
+identifyUser();
+
+let portmanteau = prompt ("Do you know what a PORTMANTEAU is? Please type Y or N");
 
 if (portmanteau == "Y"){
     alert ("Awesome! Then we are ready to get started!");
@@ -20,3 +27,12 @@ conteo = conteo -1;
 for (let i=3;i>=0;i--){
     alert(`PORMANTEAU is live in ${i}!!`);
 }
+
+function userPortmanteau(userFirstWord, userCharachter) {
+    
+    alert(`OK, ${userName}, you chose ${userFirstWord} as your first word in this PORTMANTEAU and you said you want us to build it using the last ${charachter} charachters of that word. Excellent choice!`);
+   }
+let firstWord = prompt("Let's start building this PORTMANTEAU. What word would you like to use for the first part of this PORTMANTEAU?");
+let charachter = prompt("Nice word! Now, how many charchters would you like the two words in your PORMTANTEAU to share?")
+   
+userPortmanteau (firstWord, charachter);
