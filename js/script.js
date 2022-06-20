cc644e7b6e72f12c4222297d120279a7e704824e
mm-e <magdaeaton@hotmail.com>
2022-06-20 10:45:14 -0300

    interaccion con HTML

--- js/script.js
+++ js/script.js
@@ -1,33 +1,34 @@
-let userName = prompt("Hi! My name is PT. What's your name?")
+let userName = prompt("Hi! My name is PT. What's your name?");
 function identifyUser() { //Funcion sin parametros    
-    alert(`Hi, ${userName} , Welcome!`);
+    //alert(`Hi, ${userName}! Welcome!`);
+    document.getElementById('titulo').innerHTML="<h1>PORTMANTEAU Generator</h1>"+`<h3>Hi, ${userName}! Welcome!</h3>`;
    }
 
 identifyUser();
-
+//Object Array definition
 const playingCharachters = [
-    {name: "Purply", personality: "Laidback", superpower: "Chills the unchillable"},
-    {name: "Oxno", personality: "Godlike", superpower: "All powerful"},
-    {name: "Wolf", personality: "Cunning", superpower: "Knows the future"},
-    {name: "Dodo", personality: "Outdoorsy", superpower: "Carefree"}
+    {name: "Purply", personality: "Laidback", superpower: "Chills the unchillable", photo:"../assets/****"}, //Need to add photo
+    {name: "Oxno", personality: "Godlike", superpower: "All powerful", photo:"../assets/****"}, //Need to add photo
+    {name: "Wolf", personality: "Cunning", superpower: "Knows the future", photo:"../assets/****"}, //Need to add photo
+    {name: "Dodo", personality: "Outdoorsy", superpower: "Carefree", photo:"../assets/****"} //Need to add photo
 ];
 
+//Object Array selection
+const introducePlayingCharachters = playingCharachters.map((el) => el.name);
 
-const introducePlayingCharachters = playingCharachters.map((el) => el.name)
+document.getElementById('contenido').innerHTML=`<h3>Meet the characters in our game. You will choose one to play with. Our carachters are ${introducePlayingCharachters}</h3>`;
 
-alert(`Meet the characters in our game. You will choose on to play with. Our carachters are ${introducePlayingCharachters}`)
-
-let userPlayingCharachter = prompt("Who do you want to play with today?")
+let userPlayingCharachter = prompt("Who do you want to play with today?");
 
 let selectionInArray = playingCharachters.some((el) => el.name == userPlayingCharachter);
 
 if (selectionInArray != true){
-    alert ("Playing Charachter doesn't exist")
+    alert ("Playing Charachter doesn't exist");
 }
 else{
     alert("Awesome!")
 }
-
+//Portmanteau introduction
 let portmanteau = prompt ("Do you know what a PORTMANTEAU is? Please type Y or N");
 
 if (portmanteau == "Y"){
@@ -35,7 +36,9 @@ if (portmanteau == "Y"){
 }
 
 else if (portmanteau == "N"){
-    alert ("A PORTMANTEAU is a blend of words in which parts of multiple words are combined into a new word. Here we will blend the last letters of a word you will give us with the first same letters of a word we will find for you.");
+    //alert ("A PORTMANTEAU is a blend of words in which parts of multiple words are combined into a new word. Here we will blend the last letters of a word you will give us with the first same letters of a word we will find for you.");
+    let portmanteauDefinition = "A PORTMANTEAU is a blend of words in which parts of multiple words are combined into a new word. Here we will blend the last letters of a word you will give us with the first same letters of a word we will find for you.";
+    document.getElementById('titulo').innerHTML= document.getElementById('titulo').innerHTML + `<p> ${portmanteauDefinition} </p>`;
 }
 
 else{
@@ -43,10 +46,7 @@ else{
 }
 
 
-let conteo = 3;
-conteo = conteo -1;
-
-
+//Countdown
 for (let i=3;i>=0;i--){
     alert(`PORMANTEAU is live in ${i}!!`);
 }
@@ -59,16 +59,3 @@ let firstWord = prompt("Let's start building this PORTMANTEAU. What word would y
 let charachter = prompt("Nice word! Now, how many charchters would you like the two words in your PORMTANTEAU to share?");
    
 userPortmanteau (firstWord, charachter);
-
-
-
-
-/*
-Estructura HTML del proyecto. 
-Variables de JS necesarias. 
-Funciones esenciales del proceso a simular.
-Objetos de JS
-Arrays
-Métodos de búsqueda y filtrado sobre el Array
-
-*/
